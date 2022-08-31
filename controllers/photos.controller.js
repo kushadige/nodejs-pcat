76d0b5c9de303be533819457aebee8b73d04eb71
blitzkrieg <oguzhankuslar@gmail.com>
2022-08-31 19:55:18 +0300

    Add pagination

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -2,9 +2,21 @@ import fs from 'fs';
 import Photo from '../models/photo.model.js';
 
 export const getAllPhotos = async (req, res) => {
-  const photos = await Photo.find().sort('-dateCreated');
 
-  res.render('index', { photos });
+  const page = req.query.page || 1;
+  const photosPerPage = 2;
+  const totalPhotos = await Photo.find().countDocuments();
+
+  const photos = await Photo.find()
+  .sort('-dateCreated')
+  .skip((page - 1) * photosPerPage)
+  .limit(photosPerPage);
+
+  res.render('index', { 
+    photos,
+    current: page,
+    pages: Math.ceil(totalPhotos / photosPerPage)
+  });
 };
 
 export const getPhoto = async (req, res) => {
